9d1fa2e43e3617397e7a558eb412daec32d0a0a5
Olga Dziemińska <olgadziem@gmail.com>
2021-11-13 19:40:16 +0100

    Implement printDailyFlowsAndStatesFluctuationCurveChart and printMainStatesFluctuationCurveForYears.

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import json
+import os
+
+import re
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+TEMP_FOLDER_DIRECTORY = "temp"
+chartsFontSize = 14
 
 
 def loadSettingsFromFile():
@@ -18,6 +28,102 @@ def fetch_request_data_from_UI():
     section_name_from_UI = input('Podaj przekrój rzeki')
     year_from_from_UI = input('Podaj rok, od którego operat ma być generowany')
     year_to_from_UI = input('Podaj rok, do którego operat ma być generowany')
-
+    year_of_chart_from_UI = input('Podaj rok, dla którego ma być wygenerowana krzywa wahań stanów i przepływów codziennych')
     return {'Nazwa rzeki': river_name_from_UI, 'Nazwa przekroju': section_name_from_UI, 'Rok rozpoczęcia operatu': year_from_from_UI,
-            'Rok zakończenia operatu': year_to_from_UI}
+            'Rok zakończenia operatu': year_to_from_UI,
+            'Rok dla którego ma być wygenerowana krzywa wahań stanów i przepływów codziennych': year_of_chart_from_UI}
+
+
+def printDailyFlowsAndStatesFluctuationCurveChart(przeplyw, stanWody, river_name, section_name,
+                                                  year_of_chart, dni):
+    chartFileName = "Krzywa wahań stanów i przepływów codziennych w roku %d" % year_of_chart
+    title1 = "Krzywa wahań stanów i przepływów codziennych w roku %s\n rzeka:%s przekrój:%s" % (
+        year_of_chart, river_name, section_name)
+
+    fig, axes_for_states = plt.subplots()
+    fig.subplots_adjust(right=0.75)
+    fig.set_figheight(9)  # wysokosc w
+    fig.set_figwidth(22)
+    axes_for_states.grid()  # siatka
+    axes_for_flows = axes_for_states.twinx()  # druga para osi. Oś X jest wspolna
+
+    axes_for_states.set_title(title1, size=chartsFontSize + 2)
+    axes_for_states.set_xlabel('czas [dni]', size=chartsFontSize)
+    axes_for_states.set_ylabel("Przepływ wody [$m^{3}/s$]", size=chartsFontSize)
+    axes_for_flows.set_ylabel("Stan wody [cm]", size=chartsFontSize)
+
+    axes_for_states.plot(przeplyw, "b-", lw=2, label="Stan wody")
+    axes_for_flows.plot(stanWody, "r-", lw=2, label="Przepływ")
+
+    axes_for_states.set_xlim(0, len(dni))
+    axes_for_states.set_ylim(0.2, max(przeplyw) + 50)
+    axes_for_states.set_xticks(np.arange(0, len(dni), 10))
+
+    axes_for_states.legend(loc=(0.9, 0.9))  # legenda do wykresu
+    axes_for_flows.legend(loc=(0.9, 0.86))
+
+    fig.tight_layout()
+    outputFilePath = '{tempDirectory}/{fileName}.png'.format(tempDirectory=TEMP_FOLDER_DIRECTORY,
+                                                             fileName=chartFileName)
+    fig.savefig(bbox_inches='tight', fname=outputFilePath)
+
+    return outputFilePath
+
+
+def printMainStatesFluctuationCurveForYears(main_states, main_states_2_degree, river_name, section_name,
+                                            year_from_from_UI, year_to_from_UI, ):
+    tabzm = (range(year_from_from_UI, year_to_from_UI, 1))
+    chartName = 'Krzywa wahań stanów głównych'
+    title = "%s dla okresu %s-%s\n rzeka:%s przekrój:%s" % (
+        chartName, year_from_from_UI, year_to_from_UI, river_name, section_name)
+
+    figure, axes = plt.subplots()
+    figure.subplots_adjust(right=0.75)
+    figure.set_figheight(9)
+    figure.set_figwidth(18)
+    axes.grid()
+    myFontSize = 14
+    axes.set_title(title, size=myFontSize + 3)
+    axes.set_xlabel('czas [lata]', size=myFontSize)
+    axes.set_ylabel("stan wody [$cm$]", size=myFontSize)
+
+    d = main_states['Rok']
+    c = [0]
+    c[1:] = d
+    c = pd.Series(c)
+
+    years = (range(year_from_from_UI, year_to_from_UI + 1, 1))
+    a = main_states['SW']
+    b = [2]
+    b[1:] = a
+    b = pd.Series(b)
+    axes.step(c, b, color='g', label="SW")
+    axes.plot(years, main_states['WW'], color='r', label="WW")
+    axes.plot(years, main_states['NW'], color='b', label="NW")
+    axes.legend(loc=(1.1, 0.85), fontsize=myFontSize - 3)
+    axes.set_xticks(tabzm)
+    axes.set_xlim(year_from_from_UI, year_to_from_UI)
+    highest_value = main_states[['NW', 'WW', 'SW']].max().max()
+    lowest_value = main_states[['NW', 'WW', 'SW']].min().min()
+    axes.set_ylim(lowest_value - 10, highest_value + 30)
+
+    patternsWithColors = [('.WW', 'red'), ('.SW', 'green'), ('.GS', 'violet'), ('.NW', 'blue')]
+    for columnName in main_states_2_degree:
+        state_value_in_column = main_states_2_degree[columnName][0]
+        matchingColor = 'black'
+        for pattern, color in patternsWithColors:
+            patternMatches = re.match(pattern, columnName)
+            if patternMatches:
+                matchingColor = color
+                break
+        if matchingColor == 'black':
+            print(columnName)
+        axes.axhline(y=state_value_in_column, linestyle="--", color=matchingColor)
+        horizontal_line_description = '%s (%d)' % (columnName, state_value_in_column)
+        axes.text(s=horizontal_line_description, x=years[-1] + 0.05, y=state_value_in_column)
+
+    fileName = chartName + '.png'
+    outputFilePath = os.path.join(TEMP_FOLDER_DIRECTORY, fileName)
+    figure.savefig(fname=outputFilePath)
+    figure.tight_layout()
+    return outputFilePath
--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
-from main import loadSettingsFromFile, print_greetings
+import pandas as pd
+
+from main import loadSettingsFromFile, print_greetings, printMainStatesFluctuationCurveForYears, printDailyFlowsAndStatesFluctuationCurveChart
 
 # Task 1 - load settings from file
 settingsDictionary = loadSettingsFromFile()
@@ -7,10 +9,8 @@ print(author_name)
 version = settingsDictionary['version']
 print(version)
 
-
 # Task 2 - Print greetings message with project version
 print_greetings(author_name, version)
-
 
 # Task 3 - Fetch request data from UI
 # data_from_UI = fetch_request_data_from_UI()
@@ -20,3 +20,24 @@ data_from_UI = {'Nazwa rzeki': 'Wisła', 'Nazwa przekroju': 'Tczew', 'Rok rozpoc
 river_name = data_from_UI['Nazwa rzeki']
 print(river_name)
 print(data_from_UI)
+
+# TEST printDailyFlowsAndStatesFluctuationCurveChart
+df = pd.read_csv("./testStatesAndFlows.csv", encoding='cp1250', sep='\t')
+dni = df["dzien"]
+stanWody = df["stan wody"]
+przeplyw = df["przeplyw"]
+printDailyFlowsAndStatesFluctuationCurveChart(przeplyw, stanWody, "Ganges", "Hong-Kong", 2049, dni)
+
+# TEST printMainStatesFluctuationCurveForYears
+main_states_dictionary = {'Rok': [1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991],
+                          'NW': [106, 101, 91, 97, 98, 87, 100, 90, 104, 102],
+                          'SW': [169, 142, 132, 149, 143, 127, 130, 140, 131, 136],
+                          'WW': [378, 378, 243, 377, 321, 443, 220, 376, 248, 330]}
+main_states = pd.DataFrame(main_states_dictionary)
+
+main_states_2_degree_dictionary = {'NNW': [87], 'SNW': [97.6], 'WNW': [106], 'NSW': [127], 'SSW': [139.9], 'WSW': [169],
+                                   'NWW': [220],
+                                   'SWW': [331.4], 'WWW': [443]}
+main_states_2_degree = pd.DataFrame(main_states_2_degree_dictionary)
+
+printMainStatesFluctuationCurveForYears(main_states, main_states_2_degree, 'river_name', 'section_name', 1982, 1991)
